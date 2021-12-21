b50b4b10a9a1e74f1710d776f0bb454c70746e84
Andres Guerrero <andresmgsl@gmail.com>
2021-12-21 16:28:10 +0100

    fix(library-one): missing testMessageInternal constant added

--- libs/library-one/src/lib/library-one.ts
+++ libs/library-one/src/lib/library-one.ts
@@ -1,15 +1,18 @@
 interface libraryOne {
   sayHello: () => string;
   testMessage: () => string;
+  testMessageInternal: string;
 }
 
 export function libraryOne(): libraryOne {
   const sayHello = () => 'Hello from LibraryOne.';
   const testMessage = () => 'library-one';
+  const testMessageInternal = 'text-message';
 
   return {
     sayHello,
     testMessage,
+    testMessageInternal,
   };
 }
 

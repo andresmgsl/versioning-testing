239967570b03de78e22742694a24d800c556e5f4
Andres Guerrero <andresmgsl@gmail.com>
2021-12-21 16:12:16 +0100

    test(library-four-buildable): main test deleted

--- libs/library-four-buildable/src/lib/library-four-buildable.spec.ts
+++
@@ -1,7 +0,0 @@
-import { libraryFourBuildable } from './library-four-buildable';
-
-describe('libraryFourBuildable', () => {
-  it('should work', () => {
-    expect(libraryFourBuildable()).toEqual('library-four-buildable');
-  });
-});

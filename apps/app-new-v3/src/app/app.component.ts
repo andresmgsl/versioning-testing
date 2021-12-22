c7c89e3102f60e747e28bebffdd5d75eee618022
Andres Guerrero <andresmgsl@gmail.com>
2021-12-22 10:05:37 +0100

    feat(app-new-v3): app-new-v3 created and configured

---
+++ apps/app-new-v3/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'v3-app-root',
+  template: ` <v3-app-nx-welcome></v3-app-nx-welcome> `,
+  styles: [],
+})
+export class AppComponent {
+  title = 'app-new-v3';
+}

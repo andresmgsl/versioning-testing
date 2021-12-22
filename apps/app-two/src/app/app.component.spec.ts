59297b6a34236637df7df65ba0ce4e7c11780577
Andres Guerrero <andresmgsl@gmail.com>
2021-12-22 02:29:51 +0100

    test(app-two): wrong test fixed

--- apps/app-two/src/app/app.component.spec.ts
+++ apps/app-two/src/app/app.component.spec.ts
@@ -8,12 +8,6 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
   it(`should have as title 'app-two'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;

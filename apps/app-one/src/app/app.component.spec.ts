943e5567781a66b7f5e657d34448a700ec7cdd8d
Andres Guerrero <andresmgsl@gmail.com>
2021-12-22 02:29:19 +0100

    test(app-one): updated tests

--- apps/app-one/src/app/app.component.spec.ts
+++ apps/app-one/src/app/app.component.spec.ts
@@ -13,10 +13,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'app-one'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('app-one');
-  });
 });
